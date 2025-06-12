52db07bb1257a4ac14fcc7b711a590a0470791e9
PavelVlach <vlach1989@gmail.com>
2025-06-12 11:48:22 +0200

    action/reducers for adding/removing the map

---
+++ src/client/shared/appState/reducerHandlers/mapSetRemoveMap.ts
@@ -0,0 +1,36 @@
+import { AppSharedState } from '../../appState/state.models';
+import { ActionMapRemoveFromMapSet } from '../../appState/state.models.actions';
+import { getMapSetByKey } from '../selectors/getMapSetByKey';
+import { MapSetModel } from '../../models/models.mapSet';
+
+// TODO add tests for this reducer handler
+/**
+ * Handler for map set sync change action
+ * @param state AppSharedState
+ * @param action ActionMapRemoveFromMapSet
+ * @returns Updated AppSharedState
+ */
+export const reduceHandlerMapSetRemoveMap = (
+	state: AppSharedState,
+	action: ActionMapRemoveFromMapSet
+): AppSharedState => {
+	const { payload } = action;
+
+	if (!payload) throw new Error('No payload provided for map remove from map set action');
+	const { mapSetKey, mapKey } = payload;
+
+	// Find the map set by key
+	const mapSet = getMapSetByKey(state, mapSetKey);
+	if (!mapSet) throw new Error(`MapSet with key ${mapSetKey} not found`);
+
+	// Remove the map from the map set maps
+	const updatedMapSets: MapSetModel[] = state.mapSets.map((mapSet: MapSetModel) =>
+		mapSet.key === mapSetKey ? { ...mapSet, maps: mapSet.maps.filter((map) => map !== mapKey) } : mapSet
+	);
+
+	// Remove the map from the maps array
+	const updatedMaps = state.maps.filter((map) => map.key !== mapKey);
+
+	// Return the updated state
+	return { ...state, mapSets: updatedMapSets, maps: updatedMaps };
+};
--- src/client/shared/appState/state.reducer.ts
+++ src/client/shared/appState/state.reducer.ts
@@ -10,6 +10,8 @@ import { reduceHandlerGlobalStateUpdate } from '../appState/reducerHandlers/glob
 import { reduceHandlerMapLayerActiveChange } from '../appState/reducerHandlers/mapLayerActiveChange';
 import { reduceHandlerMapLayerAdd } from '../appState/reducerHandlers/mapLayerAdd';
 import { reduceHandlerMapLayerOpacityChange } from '../appState/reducerHandlers/mapLayerOpacityChange';
+import { reduceHandlerMapSetAddMap } from '../appState/reducerHandlers/mapSetAddMap';
+import { reduceHandlerMapSetRemoveMap } from '../appState/reducerHandlers/mapSetRemoveMap';
 
 /**
  * React reducer for shared application state. Use it in useReducer react hook
@@ -18,58 +20,62 @@ import { reduceHandlerMapLayerOpacityChange } from '../appState/reducerHandlers/
  * @returns New version of application shared state
  */
 export const reducerSharedAppState = (
-    currentState: AppSharedState,
-    action: Nullable<OneOfStateActions>
+	currentState: AppSharedState,
+	action: Nullable<OneOfStateActions>
 ): AppSharedState => {
-    if (!action) return currentState;
+	if (!action) return currentState;
 
-    switch (action.type) {
-        case StateActionType.FETCH_SOURCES:
-            return {
-                ...currentState,
-                renderingLayers: currentState.renderingLayers
-                    ? [
-                            ...currentState.renderingLayers,
-                            ...parseDatasourcesToRenderingLayers(action.payload, currentState.appNode),
-                        ]
-                    : parseDatasourcesToRenderingLayers(action.payload, currentState.appNode),
-            };
-        case StateActionType.APP_NODE:
-            return { ...currentState, appNode: action.payload };
-            
-        case StateActionType.FETCH_LAYERS:
-            return { ...currentState, layers: action.payload };
-        
-        case StateActionType.FETCH_PLACES:
-            return { ...currentState, places: action.payload };
+	switch (action.type) {
+		case StateActionType.FETCH_SOURCES:
+			return {
+				...currentState,
+				renderingLayers: currentState.renderingLayers
+					? [
+							...currentState.renderingLayers,
+							...parseDatasourcesToRenderingLayers(action.payload, currentState.appNode),
+						]
+					: parseDatasourcesToRenderingLayers(action.payload, currentState.appNode),
+			};
+		case StateActionType.APP_NODE:
+			return { ...currentState, appNode: action.payload };
 
-        case StateActionType.LAYER_ACTIVE_CHANGE:
-            return reduceHandlerActiveLayerChange(currentState, action);
+		case StateActionType.FETCH_LAYERS:
+			return { ...currentState, layers: action.payload };
 
-        case StateActionType.GLOBAL_STATE_UPDATE:
-            return reduceHandlerGlobalStateUpdate(currentState, action);
+		case StateActionType.FETCH_PLACES:
+			return { ...currentState, places: action.payload };
 
-        case StateActionType.APPLY_PERSISTENT_STATE:
-            return { ...currentState, ...action.payload };
+		case StateActionType.LAYER_ACTIVE_CHANGE:
+			return reduceHandlerActiveLayerChange(currentState, action);
 
-        case StateActionType.MAP_VIEW_CHANGE:
-            return reduceHandlerMapSetMapViewChange(currentState, action);
+		case StateActionType.GLOBAL_STATE_UPDATE:
+			return reduceHandlerGlobalStateUpdate(currentState, action);
 
-        case StateActionType.MAP_LAYER_ACTIVE_CHANGE:
-            return reduceHandlerMapLayerActiveChange(currentState, action);
-            
-        case StateActionType.MAP_LAYER_ADD:
-            return reduceHandlerMapLayerAdd(currentState, action);
+		case StateActionType.APPLY_PERSISTENT_STATE:
+			return { ...currentState, ...action.payload };
 
-        case StateActionType.MAP_LAYER_OPACITY_CHANGE:
-            return reduceHandlerMapLayerOpacityChange(currentState, action);
+		case StateActionType.MAP_VIEW_CHANGE:
+			return reduceHandlerMapSetMapViewChange(currentState, action);
 
-        case StateActionType.MAP_SET_SYNC_CHANGE:
-            return reduceHandlerMapSetSyncChange(currentState, action);
+		case StateActionType.MAP_LAYER_ACTIVE_CHANGE:
+			return reduceHandlerMapLayerActiveChange(currentState, action);
 
-        default:
-            throw new Error(
-                `Shared State: Unknown action type "${(action as OneOfStateActions).type}"`
-            );
-    }
+		case StateActionType.MAP_LAYER_ADD:
+			return reduceHandlerMapLayerAdd(currentState, action);
+
+		case StateActionType.MAP_ADD_TO_MAP_SET:
+			return reduceHandlerMapSetAddMap(currentState, action);
+
+		case StateActionType.MAP_REMOVE_FROM_MAP_SET:
+			return reduceHandlerMapSetRemoveMap(currentState, action);
+
+		case StateActionType.MAP_LAYER_OPACITY_CHANGE:
+			return reduceHandlerMapLayerOpacityChange(currentState, action);
+
+		case StateActionType.MAP_SET_SYNC_CHANGE:
+			return reduceHandlerMapSetSyncChange(currentState, action);
+
+		default:
+			throw new Error(`Shared State: Unknown action type "${(action as OneOfStateActions).type}"`);
+	}
 };
