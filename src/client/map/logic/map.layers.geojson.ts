caef34d2d00790eb7836a7721826a590251b3832
Kryštof <me@krystofvavra.cz>
2025-07-14 15:54:54 +0200

    Allow geojson layer styling

--- src/client/map/logic/map.layers.geojson.ts
+++ src/client/map/logic/map.layers.geojson.ts
@@ -9,24 +9,30 @@ import { UsedDatasourceLabels } from '../../../globals/shared/panther/enums.pant
  * @param {LayerGeneralProps} param0 - The properties for creating the GeoJsonLayer.
  * @param {Object} param0.sourceNode - The source node containing configuration and key.
  * @param {boolean} param0.isActive - A flag indicating whether the layer is active.
- * @param {string} param0.key - layer identifier
+ * @param {string} param0.key - Layer identifier
  * @param {number} param0.opacity - Layer opacity
  * @returns {GeoJsonLayer} The created GeoJsonLayer instance.
  */
 export const createGeojsonLayer = ({ sourceNode, isActive, key, opacity }: LayerGeneralProps) => {
-	const { url } = validateDatasource(sourceNode, UsedDatasourceLabels.Geojson, false);
+	const { url, configurationJs } = validateDatasource(sourceNode, UsedDatasourceLabels.Geojson, true);
 
-	const layer = new GeoJsonLayer({
-		id: key,
-		opacity: opacity ?? 1,
-		visible: isActive,
-		data: url,
+	const defaultOptions = {
 		filled: true,
 		stroked: true,
 		pointRadiusScale: 0.2,
 		getPointRadius: 50,
 		getFillColor: [255, 100, 100],
 		getLineColor: [255, 100, 100],
+	};
+
+	const geojsonOptions = configurationJs?.geojsonOptions ?? defaultOptions;
+
+	const layer = new GeoJsonLayer({
+		id: key,
+		opacity: opacity ?? 1,
+		visible: isActive,
+		data: url,
+		...geojsonOptions,
 	});
 
 	return layer;
